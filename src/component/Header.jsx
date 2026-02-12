33eb81530f4e081c77382b75107601fb6daa4aaa
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-02-12 16:40:12 +0100

    desktop version design

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,17 +4,28 @@ import cartIcon from '../assets/images/icon-cart.svg'
 import profileIcon from '../assets/images/image-avatar.png'
 export function Header(){
     return(
-        <header className='w-full flex px-6 py-4 justify-between bg-White fixed'>
-            <div className='flex gap-4 items-center'>
-                <img src={menubar} alt="menu-icon" />
-                <img src={logo} alt="Logo"/>
-            </div>
-            
+        <header className='w-full grid bg-White fixed top-0 md:relative md:p-8 md:w-[90%] md:mx-auto'>
+            <div className='w-full flex px-6 py-4 justify-between '>
+                <div className='flex gap-4 items-center md:gap-8'>
+                    <img src={menubar} alt="menu-icon" className='md:hidden'/>
+                    <img src={logo} alt="Logo"/>
 
-            <div className='flex gap-4 items-center'>
-                <img src={cartIcon} alt="cart-icon" width={30}/>
-                <img src={profileIcon} alt="profile-icon" width={30}/>
-            </div>
+                    <ul className='hidden md:flex gap-6'>
+                        <li className='text-Darkgrayishblue font-md'>Collections</li>
+                        <li className='text-Darkgrayishblue font-md'>Men</li>
+                        <li className='text-Darkgrayishblue font-md'>Women</li>
+                        <li className='text-Darkgrayishblue font-md'>About</li>
+                        <li className='text-Darkgrayishblue font-md'>Contact</li>
+                    </ul>
+                </div>
+                
+
+                <div className='flex gap-4 items-center'>
+                    <img src={cartIcon} alt="cart-icon" width={30}/>
+                    <img src={profileIcon} alt="profile-icon" width={30}/>
+                </div>
+            </div> 
+            <hr className='hidden md:grid mx-8 mt-8'/>
         </header>
     )
 }
